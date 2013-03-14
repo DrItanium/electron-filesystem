ddad13e9b5ecdf8e60a068c8d3d53345c2074ab4
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-13 21:22:33 -0700

    Updated the build date string
    I need to figure out how to automate this process

--- native/constant.h
+++ native/constant.h
@@ -79,11 +79,11 @@
 #endif
 
 #ifndef CREATION_DATE_STRING
-#define CREATION_DATE_STRING "2/20/2013"
+#define CREATION_DATE_STRING "3/13/2013"
 #endif
 
 #ifndef BANNER_STRING
-#define BANNER_STRING "The Adventure Engine (Based off of CLIPS 6.30 2/20/2013)\n"
+#define BANNER_STRING "The Adventure Engine (Based off of CLIPS 6.30 3/13/2013)\n"
 #endif
 
 /*************************/
