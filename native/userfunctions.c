c77d8ae6f37547096f97026d89a0a9389dccffb6
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-04-29 00:02:28 -0700

    Added native/System/System.h
    The objective of this file is to make it really easy to add new functions
    without having to update multiple files

--- native/userfunctions.c
+++ native/userfunctions.c
@@ -43,8 +43,7 @@
 
 #include <clips.h>
 #include <binary_operations.h>
-#include <System/Platform/Platform.h>
-#include <System/Initialization/AdventureEngineInit.h>
+#include <System/System.h>
 
 void UserFunctions(void);
 void EnvUserFunctions(void *);
