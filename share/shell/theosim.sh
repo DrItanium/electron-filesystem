6df75d0c1bd4e7d888a576062d863ee6549afe8f
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-07-20 22:03:32 -0700

    Added shell scripts for the theoretical-architecture programs in the toolchain

---
+++ share/shell/theosim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#EDIT this for your purposes
+export TheoreticalFSRoot=$CLIPSPATH/src/github.com/DrItanium/theoretical-architecture
+$TheoreticalFSRoot/bin/theoretical -f2 $TheoreticalFSRoot/bin/sim.clp
